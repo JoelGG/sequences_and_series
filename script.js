7078c9166ac8ba592734684c7163022b053f4f4b
JoelGG <j.grimmer11@bristolfreeschool.org.uk>
2017-05-23 13:42:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function calc () {
+    console.log("Calc");
+     var N = document.getElementById("n").value;
+     var a = document.getElementById("a").value;
+     var r = document.getElementById("r").value;
+
+    var x = [];
+    for (var n = 0; n < N; n++) {
+        if (document.getElementById("arithmatic").checked) {
+            x[n] = (parseInt(a) + (n*parseInt(r)));
+        } else {
+            x[n] = (parseInt(a) * (Math.pow(r,n)));
+        }
+    }
+    document.getElementById("display").innerHTML = x;
+}
+
